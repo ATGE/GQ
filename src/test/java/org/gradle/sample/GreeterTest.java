1c6f3e8693cf3b8b4bd63ad7b79558e3cf5db321
German <German.Quinonez@fundacion-jala.org>
2018-02-28 10:48:00 -0400

    adding UnitTest

---
+++ src/test/java/org/gradle/sample/GreeterTest.java
@@ -0,0 +1,122 @@
+package org.gradle.sample;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by ATGE.
+ */
+public class GreeterTest {
+
+  private Greeter greeter;
+
+  /**
+   * This method tests instancing of Class Greeter.
+   */
+  @Before
+  public void setUp() {
+    greeter = new Greeter();
+  }
+
+  /**
+   * This method test output greeting is Hello AT05.
+   */
+  @Test
+  public void getGreetingCase1() throws Exception {
+    final String actualResult = "Hello AT05";
+    final String expectedResult = greeter.getGreeting();
+    assertEquals(expectedResult, actualResult);
+  }
+
+  /**
+   * This method test output greeting is Hello AT05.
+   */
+  @Test
+  public void getGreetingCase2() throws Exception {
+    final String actualResult = "Hello AT05";
+    final String expectedResult = greeter.getGreeting();
+    assertEquals(expectedResult, actualResult);
+  }
+
+  /**
+   * This method test output greeting is Hello AT05.
+   */
+  @Test
+  public void getGreetingCase3() throws Exception {
+    final String actualResult = "Hello AT05";
+    final String expectedResult = greeter.getGreeting();
+    assertEquals(expectedResult, actualResult);
+  }
+
+  /**
+   * This method test output greeting is Hello AT05.
+   */
+  @Test
+  public void getGreetingCase4() throws Exception {
+    final String actualResult = "Hello AT05";
+    final String expectedResult = greeter.getGreeting();
+    assertEquals(expectedResult, actualResult);
+  }
+
+  /**
+   * This method test output greeting is Hello AT05.
+   */
+  @Test
+  public void getGreetingCase5() throws Exception {
+    final String actualResult = "Hello AT05";
+    final String expectedResult = greeter.getGreeting();
+    assertEquals(expectedResult, actualResult);
+  }
+
+  /**
+   * This method test output greeting is Hello AT05.
+   */
+  @Test
+  public void getGreetingCase6() throws Exception {
+    final String actualResult = "Hello AT05";
+    final String expectedResult = greeter.getGreeting();
+    assertEquals(expectedResult, actualResult);
+  }
+
+  /**
+   * This method test output greeting is Hello AT05.
+   */
+  @Test
+  public void getGreetingCase7() throws Exception {
+    final String actualResult = "Hello AT05";
+    final String expectedResult = greeter.getGreeting();
+    assertEquals(expectedResult, actualResult);
+  }
+
+  /**
+   * This method test output greeting is Hello AT05.
+   */
+  @Test
+  public void getGreetingCase8() throws Exception {
+    final String actualResult = "Hello AT05";
+    final String expectedResult = greeter.getGreeting();
+    assertEquals(expectedResult, actualResult);
+  }
+
+  /**
+   * This method test output greeting is Hello AT05.
+   */
+  @Test
+  public void getGreetingCase9() throws Exception {
+    final String actualResult = "Hello AT05";
+    final String expectedResult = greeter.getGreeting();
+    assertEquals(expectedResult, actualResult);
+  }
+
+  /**
+   * This method test output greeting is Hello AT05.
+   */
+  @Test
+  public void getGreetingCase10() throws Exception {
+    final String actualResult = "Hello AT05";
+    final String expectedResult = greeter.getGreeting();
+    assertEquals(expectedResult, actualResult);
+  }
+}
